886c9cdbe68add0d4bc7aec12242aa1af9898078
Sergei Saliukov <sergei_saliukov@icloud.com>
2026-04-23 12:31:40 +0200

    Added ChatMessageBubble

---
+++ Sources/Chit-Chat/ChatMessageBubble.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public struct ChatMessageBubble: View {
+    private let message: ChatMessage
+    
+    public init(_ message: ChatMessage) {
+        self.message = message
+    }
+    
+    public var body: some View {
+        HStack {
+            if message.role == .assistant {
+                Text(message.renderedText)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+            } else {
+                Text(message.text)
+                    .padding()
+                    .background(.tint.opacity(0.15), in: .rect(cornerRadius: 20))
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+        }
+    }
+}
