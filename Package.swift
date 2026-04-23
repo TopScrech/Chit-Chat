06065815f9168d3fefba0f4a4541a338d0b0f934
Sergei Saliukov <sergei_saliukov@icloud.com>
2026-04-23 12:07:30 +0200

    Setup

--- Package.swift
+++ Package.swift
@@ -1,24 +1,14 @@
 // swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Chit-Chat",
+    name: "ChitChat",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Chit-Chat",
-            targets: ["Chit-Chat"]
-        ),
+        .library(name: "ChitChat", targets: ["ChitChat"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Chit-Chat"
-        ),
-
+        .target(name: "ChitChat")
     ],
     swiftLanguageModes: [.v6]
 )
