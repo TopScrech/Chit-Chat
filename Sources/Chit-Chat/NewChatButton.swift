433f49e73e80135b78205e37e3df7085f8aa711f
Sergei Saliukov <sergei_saliukov@icloud.com>
2026-04-23 12:48:08 +0200

    Added NewChatButton

---
+++ Sources/Chit-Chat/NewChatButton.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 9, *)
+public struct NewChatButton: View {
+    private let disabled: Bool
+    private let action: () -> ()
+    
+    public init(disabled: Bool, action: @escaping () -> Void) {
+        self.disabled = disabled
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button("New Chat", systemImage: "square.and.pencil", action: action)
+            .disabled(disabled)
+    }
+}
