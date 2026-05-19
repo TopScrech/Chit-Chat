3364222fe76b967e541c688101e93efbf7c989d8
TopScrech <sergei_saliukov@icloud.com>
2026-05-19 09:40:13 +0200

    upgrade

--- Package.swift
+++ Package.swift
@@ -16,10 +16,7 @@ let package = Package(
         .package(url: "https://github.com/TopScrech/ScrechKit", from: "1.0.0")
     ],
     targets: [
-        .target(
-            name: "ChitChat",
-            dependencies: ["ScrechKit"]
-        )
+        .target(name: "ChitChat", dependencies: ["ScrechKit"])
     ],
     swiftLanguageModes: [.v6]
 )
